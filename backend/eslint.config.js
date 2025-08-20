c17e2340a9d9c1b7fd736ec0095589dda29b128d
Pratik Kulkarni <pnkulkarni@pm.me>
2025-08-20 13:00:33 +0000

    run eslint fix for backend

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,32 +1,3 @@
-// import js from "@eslint/js";
-// import globals from "globals";
-// import tseslint from "typescript-eslint";
-// import { globalIgnores } from "eslint/config";
-// import stylistic from "@stylistic/eslint-plugin";
-//
-// export default tseslint.config([
-//     globalIgnores(["dist"]),
-//
-//     stylistic.configs.customize({
-//         quotes: "double",
-//         semi: true,
-//         indent: 4,
-//         jsx: true,
-//     }),
-//
-//     {
-//         files: ["**/*.{ts}"],
-//         extends: [
-//             js.configs.recommended,
-//             tseslint.configs.recommended,
-//         ],
-//         languageOptions: {
-//             ecmaVersion: 2020,
-//             globals: globals.browser,
-//         },
-//     },
-// ]);
-
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
@@ -34,7 +5,7 @@ import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config([
     {
-        ignores: ["dist"],
+        ignores: ["dist", "generated"],
     },
 
     stylistic.configs.customize({
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,17 +2,19 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { auth } from "./lib/auth.js";
-import {authMiddleware, protectRoute} from "./middleware.js";
+import { authMiddleware, protectRoute } from "./middleware.js";
 
 const app = new Hono();
 
 app.use("*", logger());
 app.use("*", authMiddleware);
 
+type AuthResponse = { message: string };
+
 app.basePath("/api").get("/healthcheck", (c) => {
     return c.text("aye aye cheif");
 }).get("/me", protectRoute, (c) => {
-    return c.json({ message: "you are authenticated" }, 200);
+    return c.json<AuthResponse>({ message: "you are authenticated" }, 200);
 }).on(["POST", "GET"], "/auth/*", async (c) => {
     return auth.handler(c.req.raw);
 });
